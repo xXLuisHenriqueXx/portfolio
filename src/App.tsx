73723906e5c82379519d5c81ae46abc65fde2c86
xXLuisHenriqueXx <luishenri567@gmail.com>
2026-08-22 10:35:43 -0300

    FEAT: add lazy loading in heavy components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { lazy, Suspense, useState } from "react";
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+const About = lazy(() => import("./components/About"));
+const Skills = lazy(() => import("./components/Skills"));
+const Projects = lazy(() => import("./components/Projects"));
+const Contact = lazy(() => import("./components/Contact"));
 
 export type TActiveScreen =
   | "home"
@@ -20,16 +20,14 @@ function App() {
   return (
     <main className={"flex flex-col min-w-full overflow-x-hidden"}>
       <Navbar activeScreen={activeScreen} />
-
       <Home setActiveScreen={setActiveScreen} />
 
-      <About setActiveScreen={setActiveScreen} />
-
-      <Skills setActiveScreen={setActiveScreen} />
-
-      <Projects setActiveScreen={setActiveScreen} />
-
-      <Contact setActiveScreen={setActiveScreen} />
+      <Suspense fallback={null}>
+        <About setActiveScreen={setActiveScreen} />
+        <Skills setActiveScreen={setActiveScreen} />
+        <Projects setActiveScreen={setActiveScreen} />
+        <Contact setActiveScreen={setActiveScreen} />
+      </Suspense>
     </main>
   );
 }
