bdbcd7c82637ff925cae477a5d12e6f8c10fcc6f
xXLuisHenriqueXx <luishenri567@gmail.com>
2026-08-24 16:34:33 -0300

    FEAT: add exit animation in slidein component

--- src/components/Navbar/MobileMenu.tsx
+++ src/components/Navbar/MobileMenu.tsx
@@ -8,6 +8,7 @@ import SlideIn from "@src/components/ui/animations/SlideIn";
 import { cn } from "@src/lib/utils";
 import useDismiss from "@src/hooks/useDismiss";
 import type { NavbarLink } from "@src/static/data/Navbar.data";
+import { AnimatePresence } from "motion/react";
 
 interface Props {
   open: boolean;
@@ -22,40 +23,42 @@ const Modal = ({ open, onClose, active, items }: Props) => {
   const ref = useRef<HTMLDivElement | null>(null);
   useDismiss({ ref, enabled: open, onDismiss: onClose });
 
-  if (!open) return null;
-
   return (
-    <SlideIn
-      className="absolute top-20 right-6"
-      direction="right"
-      aria-label={t("navbar-a11y.mobileNavigation")}
-    >
-      <Card ref={ref} className="min-w-56">
-        <nav className="flex flex-col gap-y-2">
-          {items.map(({ id, text, icon: Icon, action }) => {
-            const isActive = active === id;
-            return (
-              <Button
-                key={id}
-                className={cn("relative w-full px-8", {
-                  "bg-primary/10 text-primary": isActive,
-                })}
-                variant="ghost"
-                onClick={action}
-                role="menuitem"
-              >
-                <Icon size={16} aria-hidden />
-                {t(text)}
+    <AnimatePresence mode="wait">
+      {open && (
+        <SlideIn
+          className="absolute top-20 right-6"
+          direction="right"
+          aria-label={t("navbar-a11y.mobileNavigation")}
+        >
+          <Card ref={ref} className="min-w-56">
+            <nav className="flex flex-col gap-y-2">
+              {items.map(({ id, text, icon: Icon, action }) => {
+                const isActive = active === id;
+                return (
+                  <Button
+                    key={id}
+                    className={cn("relative w-full px-8", {
+                      "bg-primary/10 text-primary": isActive,
+                    })}
+                    variant="ghost"
+                    onClick={action}
+                    role="menuitem"
+                  >
+                    <Icon size={16} aria-hidden />
+                    {t(text)}
 
-                {isActive && (
-                  <span className="absolute top-1/2 right-4 -translate-y-1/2 size-2 bg-primary rounded-full" />
-                )}
-              </Button>
-            );
-          })}
-        </nav>
-      </Card>
-    </SlideIn>
+                    {isActive && (
+                      <span className="absolute top-1/2 right-4 -translate-y-1/2 size-2 bg-primary rounded-full" />
+                    )}
+                  </Button>
+                );
+              })}
+            </nav>
+          </Card>
+        </SlideIn>
+      )}
+    </AnimatePresence>
   );
 };
 
--- src/components/ui/animations/SlideIn.tsx
+++ src/components/ui/animations/SlideIn.tsx
@@ -2,6 +2,7 @@ import type { PropsWithChildren } from "react";
 import { motion, type DOMMotionComponents } from "motion/react";
 
 type MotionTags = keyof DOMMotionComponents;
+
 interface Props extends PropsWithChildren {
   as?: MotionTags;
   delay?: number;
@@ -18,11 +19,18 @@ const SlideIn = ({
 }: Props) => {
   const Component = motion[as];
 
+  const x = direction === "left" ? -400 : 400;
+
   return (
     <Component
-      initial={{ translateX: direction === "left" ? -200 : 200 }}
-      whileInView={{ translateX: 0 }}
-      transition={{ duration: 0.5, type: "spring", delay }}
+      initial={{ x }}
+      animate={{ x: 0 }}
+      exit={{ x }}
+      transition={{
+        duration: 0.75,
+        type: "spring",
+        delay,
+      }}
       {...props}
     >
       {children}
