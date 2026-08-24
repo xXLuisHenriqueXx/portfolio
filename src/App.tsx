325002bf70e7bc0945abcd0718842e9f0c3a4ffd
xXLuisHenriqueXx <luishenri567@gmail.com>
2026-08-24 15:30:29 -0300

    FEAT: add logoloop component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import LogoLoop from "./components/ui/LogoLoop";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -6,15 +7,27 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 import { SECTION_IDS, useActiveSection } from "./hooks/useActiveSection";
+import { LOOP_LOGOS } from "./static/data/LogoLoop.data";
 
 function App() {
   const activeScreen = useActiveSection(SECTION_IDS);
 
   return (
-    <main className={"flex flex-col min-w-full overflow-x-hidden"}>
+    <main className="relative flex flex-col min-w-full overflow-hidden">
       <Navbar activeScreen={activeScreen} />
 
       <Home />
+
+      <div className="relative h-40 text-primary [&_svg]:size-12 overflow-hidden">
+        <LogoLoop
+          logos={LOOP_LOGOS}
+          direction="left"
+          gap={64}
+          fadeOut
+          fadeOutColor="var(--background)"
+        />
+      </div>
+
       <About />
       <Skills />
       <Projects />
